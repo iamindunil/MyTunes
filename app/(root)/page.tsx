ca9c3791aaf7efa4ed19513989ef77a74606e640
Indunil Kavinda <indunilkavinda1121@gmail.com>
2026-01-08 00:18:03 +0530

    initial commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,65 +1,99 @@
-import Image from "next/image";
+"use client"
+import AlbumCard from "@/components/cards/album";
+import ArtistCard from "@/components/cards/artist";
+import SongCard from "@/components/cards/song";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Skeleton } from "@/components/ui/skeleton";
+import { getSongsByQuery, searchAlbumByQuery } from "@/lib/fetch";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+export default function Page() {
+  const [latest, setLatest] = useState([]);
+  const [popular, setPopular] = useState([]);
+  const [albums, setAlbums] = useState([]);
+
+  const getSongs = async (e, type) => {
+    const get = await getSongsByQuery(e);
+    const data = await get.json();
+    if (type === "latest") {
+      setLatest(data.data.results);
+    } else if (type === "popular") {
+      setPopular(data.data.results);
+    }
+  };
+
+  const getAlbum = async () => {
+    const get = await searchAlbumByQuery("latest");
+    const data = await get.json();
+    setAlbums(data.data.results);
+  };
+
+  useEffect(() => {
+    getSongs("latest", "latest");
+    getSongs("trending", "popular");
+    getAlbum();
+  }, []);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="px-6 py-5 md:px-20 lg:px-32">
+      <div>
+        <div className="grid">
+          <h1 className="text-base">Songs</h1>
+          <p className="text-xs text-muted-foreground">Top new released songs.</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+        <ScrollArea className="rounded-md mt-4">
+          <div className="flex gap-4">
+            {latest.length ? latest.slice().map((song) => (
+              <SongCard key={song.id} image={song.image[2].url} album={song.album} title={song.name} artist={song.artists.primary[0].name} id={song.id} />
+            )) : Array.from({ length: 10 }).map((_, i) => <SongCard key={i} />)}
+          </div>
+          <ScrollBar orientation="horizontal" className="hidden sm:flex" />
+        </ScrollArea>
+      </div>
+
+      <div className="mt-14">
+        <h1 className="text-base">Albums</h1>
+        <p className="text-xs text-muted-foreground">Top new released albums.</p>
+        <ScrollArea className="rounded-md mt-6">
+          <div className="flex gap-4">
+            {albums.length ? albums.slice().map((song) => (
+              <AlbumCard key={song.id} lang={song.language} image={song.image[2].url} album={song.album} title={song.name} artist={song.artists.primary[0].name} id={`album/${song.id}`} />
+            )) : Array.from({ length: 10 }).map((_, i) => <SongCard key={i} />)}
+          </div>
+          <ScrollBar orientation="horizontal" className="hidden sm:flex" />
+        </ScrollArea>
+      </div>
+
+      <div className="mt-12">
+        <h1 className="text-base">Artists</h1>
+        <p className="text-xs text-muted-foreground">Most searched artists.</p>
+        <ScrollArea className="rounded-md mt-6">
+          <div className="flex gap-4">
+            {latest.length ? [...new Set(latest.map(a => a.artists.primary[0].id))].map(id => (
+              <ArtistCard key={id} id={id} image={latest.find(a => a.artists.primary[0].id === id).artists.primary[0].image[2]?.url || `https://az-avatar.vercel.app/api/avatar/?bgColor=0f0f0f0&fontSize=60&text=${latest.find(a => a.artists.primary[0].id === id).artists.primary[0].name.split("")[0].toUpperCase() || "UN"}`} name={latest.find(a => a.artists.primary[0].id === id).artists.primary[0].name} />
+            )) : Array.from({ length: 10 }).map((_, i) => (
+              <div key={i} className="grid gap-2">
+                <Skeleton className="h-[100px] w-[100px] rounded-full" />
+                <Skeleton className="h-3 w-20" />
+              </div>
+            ))}
+          </div>
+          <ScrollBar orientation="horizontal" className="hidden sm:flex" />
+        </ScrollArea>
+      </div>
+
+      <div className="mt-12">
+        <h1 className="text-base">Trending</h1>
+        <p className="text-xs text-muted-foreground">Most played songs in this week.</p>
+        <ScrollArea className="rounded-md mt-6">
+          <div className="flex gap-4">
+            {popular.length ? popular.map((song) => (
+              <SongCard key={song.id} id={song.id} image={song.image[2].url} title={song.name} artist={song.artists.primary[0].name} />
+            )) : Array.from({ length: 10 }).map((_, i) => <SongCard key={i} />)}
+          </div>
+          <ScrollBar orientation="horizontal" className="hidden sm:flex" />
+        </ScrollArea>
+      </div>
+    </main>
+  )
 }
